2dc86aca18a21d37a6d5e38604a477e2ceb4e18b
JoaoVSouto <joaovitorss99@hotmail.com>
2020-09-19 11:19:57 -0300

    Configures sass-resources-loader

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,17 @@
 <template>
-  <router-view/>
+  <RouterView />
+
+  <div id="hello-world">
+    <h1>hello mundo</h1>
+  </div>
 </template>
 
-<style lang="scss"></style>
+<style lang="scss" scoped>
+#hello-world {
+  color: $primary;
+
+  @include media('>=tablet') {
+    background-color: #ff0;
+  }
+}
+</style>
--- vue.config.js
+++ vue.config.js
@@ -1,13 +1,22 @@
 module.exports = {
   chainWebpack(config) {
-    config
-      .plugin('html')
-      .tap((args) => {
-        const [firstArgs] = args;
+    config.plugin('html').tap((args) => {
+      const [firstArgs] = args;
 
-        firstArgs.title = 'GH Jobs Explorer';
+      firstArgs.title = 'GH Jobs Explorer';
 
-        return args;
-      });
+      return args;
+    });
+
+    const oneOfsMap = config.module.rule('scss').oneOfs.store;
+    oneOfsMap.forEach((item) => {
+      item
+        .use('sass-resources-loader')
+        .loader('sass-resources-loader')
+        .options({
+          resources: ['./src/styles/_colors.scss', './src/styles/_mixins.scss'],
+        })
+        .end();
+    });
   },
 };
