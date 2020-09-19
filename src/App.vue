9e50a66c3830f70b883c317f8d7e28cf4276ce02
JoaoVSouto <joaovitorss99@hotmail.com>
2020-09-19 12:24:33 -0300

    Styles app container for spacing

--- src/App.vue
+++ src/App.vue
@@ -15,3 +15,15 @@ export default defineComponent({
   },
 });
 </script>
+
+<style lang="scss">
+#app {
+  max-width: 1176px;
+  padding: 0 12px;
+  margin: 12px auto;
+
+  @include media('>=tablet') {
+    margin: 32px auto;
+  }
+}
+</style>
