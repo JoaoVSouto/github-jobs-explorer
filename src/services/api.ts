d0f6c54aea49c21449c1470a5d2feb01211ae771
JoaoVSouto <joaovitorss99@hotmail.com>
2021-06-04 10:46:12 -0300

    Change cors anywhere service to all origins

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,8 +1,17 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://cors-anywhere.herokuapp.com/https://jobs.github.com',
+  baseURL: 'https://api.allorigins.win/get?url=https://jobs.github.com',
   // baseURL: 'https://api.jsonbin.io/b/5f68a8457243cd7e8240a821',
+});
+
+api.interceptors.response.use((response) => {
+  const data = JSON.parse(response.data.contents);
+
+  // eslint-disable-next-line no-param-reassign
+  response.data = data;
+
+  return response;
 });
 
 export default api;
