0379fc6cd892862894751e0382c71afbd9473aa6
JoaoVSouto <joaovitorss99@hotmail.com>
2021-06-04 11:05:52 -0300

    Fix query parameters format

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,14 +1,27 @@
+/* eslint-disable no-param-reassign */
 import axios from 'axios';
+import { QueryValue, stringifyQuery } from 'ufo';
 
 const api = axios.create({
   baseURL: 'https://api.allorigins.win/get?url=https://jobs.github.com',
   // baseURL: 'https://api.jsonbin.io/b/5f68a8457243cd7e8240a821',
 });
 
+api.interceptors.request.use((config) => {
+  const queryWithTruthyValues = Object.fromEntries(
+    Object.entries(config.params).filter(([_, value]) => Boolean(value)),
+  ) as Record<string, QueryValue>;
+  const stringifiedQuery = stringifyQuery(queryWithTruthyValues);
+
+  config.params = {};
+  config.url = `${config.url}?=${stringifiedQuery}`;
+
+  return config;
+});
+
 api.interceptors.response.use((response) => {
   const data = JSON.parse(response.data.contents);
 
-  // eslint-disable-next-line no-param-reassign
   response.data = data;
 
   return response;
